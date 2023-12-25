8e72e1234cb824762cae65e7dd156470b6f1f44f
Yun YeoJin <duwls0349@naver.com>
2023-12-25 20:23:17 +0900

    [Chore] 주석 내용 삭제

--- Programmers_Coding_Test/피보나치 수.swift
+++ Programmers_Coding_Test/피보나치 수.swift
@@ -17,25 +17,6 @@ import Foundation
  2 이상의 n이 입력되었을 때, n번째 피보나치 수를 1234567으로 나눈 나머지를 리턴하는 함수, solution을 완성해 주세요.
  */
 //재귀함수로 풀면 안되는 듯. => 시간초과 에러 발생
-//func solution37(_ n:Int) -> Int {
-//    
-//    var result = 0
-//    
-//    for i in 0..<n - 1 {
-//        result = resultBack(i)
-//    }
-//    
-//    return result % 1234567
-//}
-//
-//func resultBack(_ num: Int) -> Int {
-//    if num >= 1 {
-//        return resultBack(num - 1) + resultBack(num - 2)
-//    } else {
-//        return 1
-//    }
-//}
-
 func solution37(_ n:Int) -> Int {
     var result: [Int] = [0, 1]
     guard n > 1 else { return n }
