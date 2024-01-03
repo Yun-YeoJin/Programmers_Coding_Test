3a0bea8584bf633f04752b90750bd3e595638bd1
Yun-YeoJin <duwls0349@naver.com>
2024-01-03 14:39:15 +0900

    LV1. K번째수

---
+++ Programmers_Coding_Test/K번째수.swift
@@ -0,0 +1,41 @@
+//
+//  K번째수.swift
+//  Programmers_Coding_Test
+//
+//  Created by 윤여진 on 1/3/24.
+//
+
+import Foundation
+/**
+ 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수를 구하려 합니다.
+
+ 예를 들어 array가 [1, 5, 2, 6, 3, 7, 4], i = 2, j = 5, k = 3이라면
+
+ array의 2번째부터 5번째까지 자르면 [5, 2, 6, 3]입니다.
+ 1에서 나온 배열을 정렬하면 [2, 3, 5, 6]입니다.
+ 2에서 나온 배열의 3번째 숫자는 5입니다.
+ 배열 array, [i, j, k]를 원소로 가진 2차원 배열 commands가 매개변수로 주어질 때, commands의 모든 원소에 대해 앞서 설명한 연산을 적용했을 때 나온 결과를 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+ */
+
+func solution44(_ array:[Int], _ commands:[[Int]]) -> [Int] {
+    
+    var result: [Int] = []
+    
+    for i in 0 ..< commands.count {
+        
+        var arr: [Int] = []
+        
+        //i부터 j순서까지 자른 배열
+        for j in (commands[i][0] - 1)...(commands[i][1] - 1){
+            arr.append(array[j])
+        }
+        
+        //정렬하기
+        arr.sort()
+        
+        //k번째 수
+        result.append(arr[commands[i][2]-1])
+    }
+    
+    return result
+}
--- Programmers_Coding_Test/main.swift
+++ Programmers_Coding_Test/main.swift
@@ -7,4 +7,4 @@
 
 import Foundation
 
-print(solution43(["sun", "bed", "car"], 1))
+print(solution44([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
